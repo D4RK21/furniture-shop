dbe0df3540f328f5e1da07aaee3d7ec8a15bada6
Lefend12 <mrtiggera@gmail.com>
2022-03-27 00:10:11 +0300

    delete bootstarp

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "@/assets/scss/main.scss";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
 
 const app = createApp(App);
 
@@ -15,8 +12,6 @@ files.keys().map(function (key) {
   app.component(key.split("/").pop().split(".")[0], files(key).default);
 });
 
-app.use(BootstrapVue);
-app.use(IconsPlugin);
 app.use(store);
 app.use(router);
 app.mount("#app");
