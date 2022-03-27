7bf7711fb13de1dd4ec5a4e7057ee5df6de8074e
D4RK21 <zhmaytsevm@gmail.com>
2022-03-27 14:55:25 +0300

    Add cart

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,9 +1,169 @@
-<template></template>
+<template>
+  <div class="modal-wrapper">
+    <div class="modal-content">
+      <div class="cart-header">
+        <div class="cart-title">Корзина</div>
+        <div class="cart-close">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="25" height="25">
+            <g clip-path="url(#__lottie_element_43)">
+              <g transform="matrix(1,0,0,1,0,0)" opacity="1" style="display: block">
+                <g opacity="1" transform="matrix(1,0,0,1,25,25)">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="miter"
+                    fill-opacity="0"
+                    stroke-miterlimit="10"
+                    stroke="rgb(0,0,0)"
+                    stroke-opacity="1"
+                    stroke-width="4"
+                    d=" M-15.61400032043457,-15.61299991607666 C-15.61400032043457,-15.61299991607666 15.61400032043457,15.61400032043457 15.61400032043457,15.61400032043457"
+                  ></path>
+                </g>
+                <g opacity="1" transform="matrix(0,-1,1,0,25,25)">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="miter"
+                    fill-opacity="0"
+                    stroke-miterlimit="10"
+                    stroke="rgb(0,0,0)"
+                    stroke-opacity="1"
+                    stroke-width="4"
+                    d=" M-15.61400032043457,-15.61299991607666 C-15.61400032043457,-15.61299991607666 15.61400032043457,15.61400032043457 15.61400032043457,15.61400032043457"
+                  ></path>
+                </g>
+              </g>
+              <g transform="matrix(1,0,0,1,0,0)" opacity="1" style="display: block">
+                <g opacity="1" transform="matrix(1,0,0,1,24.33099937438965,30)">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="miter"
+                    fill-opacity="0"
+                    stroke-miterlimit="10"
+                    stroke="rgb(0,0,0)"
+                    stroke-opacity="1"
+                    stroke-width="4"
+                    d="M0 0"
+                  ></path>
+                </g>
+              </g>
+              <g style="display: none" transform="matrix(1,0,0,1,0,0)" opacity="1">
+                <g opacity="1" transform="matrix(1,0,0,1,11.501999855041504,15)">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="miter"
+                    fill-opacity="0"
+                    stroke-miterlimit="10"
+                    stroke="rgb(0,0,0)"
+                    stroke-opacity="1"
+                    stroke-width="4"
+                    d=" M5.448930740356445,3.2294015884399414 C5.448930740356445,3.2294015884399414 -5.445000171661377,0 -5.445000171661377,0"
+                  ></path>
+                </g>
+                <g opacity="1" transform="matrix(1,0,0,1,11.501999855041504,15)">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="miter"
+                    fill-opacity="0"
+                    stroke-miterlimit="10"
+                    stroke="rgb(0,0,0)"
+                    stroke-opacity="1"
+                    stroke-width="4"
+                    d=" M-5.445000171661377,0 C-5.445000171661377,0 5.4506354331970215,-3.1850228309631348 5.4506354331970215,-3.1850228309631348"
+                  ></path>
+                </g>
+              </g>
+              <g style="display: none" transform="matrix(0,0,0,1,25,0)" opacity="1">
+                <g
+                  opacity="1"
+                  transform="matrix(0.7071067690849304,-0.7071067690849304,0.7071067690849304,0.7071067690849304,25,25)"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="miter"
+                    fill-opacity="0"
+                    stroke-miterlimit="10"
+                    stroke="rgb(0,0,0)"
+                    stroke-opacity="1"
+                    stroke-width="4"
+                    d=" M-15.61400032043457,-15.61299991607666 C-15.61400032043457,-15.61299991607666 15.61400032043457,15.61400032043457 15.61400032043457,15.61400032043457"
+                  ></path>
+                </g>
+                <g
+                  opacity="1"
+                  transform="matrix(0.7071067690849304,-0.7071067690849304,0.7071067690849304,0.7071067690849304,25,25)"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="miter"
+                    fill-opacity="0"
+                    stroke-miterlimit="10"
+                    stroke="rgb(0,0,0)"
+                    stroke-opacity="1"
+                    stroke-width="4"
+                    d=" M-15.61400032043457,-15.61299991607666 C-15.61400032043457,-15.61299991607666 15.61400032043457,15.61400032043457 15.61400032043457,15.61400032043457"
+                  ></path>
+                </g>
+              </g>
+              <g style="display: none" transform="matrix(1,0,0,1,0,0)" opacity="1">
+                <g opacity="1" transform="matrix(1,0,0,1,24.33099937438965,30)">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="miter"
+                    fill-opacity="0"
+                    stroke-miterlimit="10"
+                    stroke="rgb(0,0,0)"
+                    stroke-opacity="1"
+                    stroke-width="4"
+                    d=" M13.668999671936035,15 C13.668999671936035,15 17.66900062561035,8.654999732971191 17.66900062561035,0 C17.66900062561035,-8.755999565124512 12.024999618530273,-15 -0.3310000002384186,-15 C-0.3310000002384186,-15 -17.66900062561035,-15 -17.66900062561035,-15"
+                  ></path>
+                </g>
+              </g>
+              <g style="display: none" transform="matrix(1,0,0,1,0,0)" opacity="1">
+                <g opacity="1" transform="matrix(1,0,0,1,11.501999855041504,15)">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="miter"
+                    fill-opacity="0"
+                    stroke-miterlimit="10"
+                    stroke="rgb(0,0,0)"
+                    stroke-opacity="1"
+                    stroke-width="4"
+                    d=" M5.443999767303467,10.890000343322754 C5.443999767303467,10.890000343322754 -5.445000171661377,0 -5.445000171661377,0"
+                  ></path>
+                </g>
+                <g opacity="1" transform="matrix(1,0,0,1,11.501999855041504,15)">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="miter"
+                    fill-opacity="0"
+                    stroke-miterlimit="10"
+                    stroke="rgb(0,0,0)"
+                    stroke-opacity="1"
+                    stroke-width="4"
+                    d=" M-5.445000171661377,0 C-5.445000171661377,0 5.445000171661377,-10.890000343322754 5.445000171661377,-10.890000343322754"
+                  ></path>
+                </g>
+              </g>
+            </g>
+          </svg>
+        </div>
+      </div>
+      <section class="items-in-cart">
+        <div class="section-title">Товары в корзине</div>
+        <div class="item"></div>
+      </section>
+      <section class="checkout">
+        <div class="section-title">Оформить заказ</div>
+        <input type="text" class="checkout-input" placeholder="Ваше имя" />
+        <input type="text" class="checkout-input" placeholder="Телефон" />
+        <input type="text" class="checkout-input" placeholder="Адрес" />
+      </section>
+    </div>
+  </div>
+</template>
 
 <script>
-export default {
-  props: ["items"],
-};
+export default {};
 </script>
 
 <style></style>
